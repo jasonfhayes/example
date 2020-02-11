dfcdd283dbd13151c249c300311ac592e9855e88
jason hayes <jason_hayes@me.com>
2020-02-10 16:20:53 -0800

    first commit

---
+++ diceNonOOPJason.py
@@ -0,0 +1,10 @@
+import random
+my_dict = {2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0, 11: 0, 12: 0}
+how_many = int(input('How many rolls? '))
+for i in range(how_many):
+	total = (random.randrange(1, 7)) + (random.randrange(1, 7))
+	my_dict[total] = my_dict[total] + 1
+print('\nTotal:\tCount:\tPercetage:')
+for i in range(2, 13):
+	special_number = ('{:.1f}'.format((my_dict[i]/how_many)*100))
+	print(f'{i}:\t{my_dict[i]}\t{special_number}%')
