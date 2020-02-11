fe385c2d1583dd3ea4fec82d0105a9d001346d49
jason hayes <jason_hayes@me.com>
2020-02-10 16:40:58 -0800

    world > worlds

--- diceNonOOPJason.py
+++ diceNonOOPJason.py
@@ -9,4 +9,4 @@ for i in range(2, 13):
 	my_very_special_number = ('{:.1f}'.format((my_dict[i]/how_many)*100))
 	print(f'{i}:\t{my_dict[i]}\t{my_very_special_number}%')
 	
-## Hello World
+## Hello Worlds
